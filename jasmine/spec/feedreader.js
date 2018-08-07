dc0e4803475babc7c0a0a8381b58f5d49766b54c
Felix Leboo <felixlemi@gmail.com>
2018-08-07 12:56:49 -0500

    Declared variable to be used by 'The menu' test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -61,6 +61,13 @@ $(function() {
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
 
+        var bodyTag;
+
+        //declares and assigns value to each test within "The menu" test suite
+        beforeEach(function() {
+            bodyTag = document.getElementsByTagName("BODY")[0];
+        });
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -144,12 +151,11 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-         
+
         // compare the two selections against each other
         it('content changes', function(done) {
             expect(initialContent).not.toBe(changedContent);
             done();
         });
-
     });
 }());
